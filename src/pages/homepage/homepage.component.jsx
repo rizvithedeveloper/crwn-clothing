f8d0e2333b6112c164ce03a0939f9e58a8b69b83
Muhammad Rizwan <rizwan.xammin@gmail.com>
2021-05-05 01:38:56 +0500

    Added Router toapplication

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const HomePage = () => {
+const HomePage = (props) => {
     return (
         <div className="homepage">
+            {
+                console.log(props)
+            }
             <Directory />
         </div>
     )
